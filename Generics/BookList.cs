cd2c7cd2a16bc6fba2ee5c42d5a44a6ef751f4dd
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-03-30 11:51:30 -0500

    updated notes and codes

---
+++ Generics/BookList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class BookList
+    {   
+        public void Add(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Book this[int index]
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+
+        public class ObjectList
+        {
+            public void Add(object value)
+            {
+
+            }
+
+            public object this[int index]
+            {
+                get { throw new NotImplementedException(); }
+            }
+        }
+
+
+    }
+}
